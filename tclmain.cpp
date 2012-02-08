0bebff5d8334fa48909332816da4f5b562786a7e
Edward Brekelbaum <nedbrek@yahoo.com>
2012-02-07 20:21:55 -0500

    Changed "verse" command to "get" Changed "get" to return list of dict Implemented "get"

--- tclmain.cpp
+++ tclmain.cpp
@@ -1,13 +1,20 @@
 #include "tclparms.h"
 #include <sword/swmgr.h>
 #include <sword/swmodule.h>
+#include <sword/versekey.h>
+
+struct SwordData
+{
+	sword::SWMgr library;
+};
 
 static
 int modulesCmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
 {
+	sword::SWMgr &library = reinterpret_cast<SwordData*>(cdata)->library;
+
 	Tcl_Obj *ret = Tcl_NewListObj(0, NULL);
 
-	sword::SWMgr library;
 	const char *keyName = "Name";
 	const char *keyType = "Type";
 	for(sword::ModMap::const_iterator i = library.Modules.begin(); i != library.Modules.end(); ++i)
@@ -28,10 +35,60 @@ int modulesCmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const ob
 	return TCL_OK;
 }
 
+static
+int getVerseCmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+{
+	if (objc < 3)
+	{
+		Tcl_AddErrorInfo(interp, "Usage: sword::get <module> <verse>");
+		return TCL_ERROR;
+	}
+
+	sword::SWMgr &library = reinterpret_cast<SwordData*>(cdata)->library;
+	Parms p(interp, objv, unsigned(objc));
+
+	sword::ModMap::const_iterator modI = library.Modules.find(p.getStringParm(0));
+	if (modI == library.Modules.end())
+	{
+		Tcl_AddErrorInfo(interp, "Module not found");
+		return TCL_ERROR;
+	}
+
+	sword::SWModule *mod = modI->second;
+	Tcl_Obj *ret = Tcl_NewListObj(0, NULL);
+
+	const char *keyRef = "Ref";
+	const char *keyTxt = "Text";
+	sword::ListKey l = sword::VerseKey().ParseVerseList(p.getStringParm(1), 0, true);
+	for(; !l.Error(); l++)
+	{
+		const char *valRef = l.getText();
+		mod->setKey(valRef);
+		const char *valTxt = mod->RenderText();
+
+		Tcl_Obj *entry = Tcl_NewDictObj();
+		Tcl_DictObjPut(interp, entry, Tcl_NewStringObj(keyRef, strlen(keyRef)),
+		    Tcl_NewStringObj(valRef, strlen(valRef)));
+		Tcl_DictObjPut(interp, entry, Tcl_NewStringObj(keyTxt, strlen(keyTxt)),
+		    Tcl_NewStringObj(valTxt, strlen(valTxt)));
+
+		Tcl_ListObjAppendElement(interp, ret, entry);
+	}
+
+	Tcl_SetObjResult(interp, ret);
+
+	return TCL_OK;
+}
+
 extern "C" int Tclsword_Init(Tcl_Interp *interp)
 {
 	Tcl_Namespace *ns = Tcl_CreateNamespace(interp, "sword", NULL, NULL);
-	Tcl_CreateObjCommand(interp, "sword::modules", modulesCmd, NULL, NULL);
+
+	SwordData *data = new SwordData;
+
+	Tcl_CreateObjCommand(interp, "sword::modules", modulesCmd, data, NULL);
+	Tcl_CreateObjCommand(interp, "sword::get", getVerseCmd, data, NULL);
+
 	return TCL_OK;
 }
 
