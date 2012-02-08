03e511e106de7e1d487d44e33b5accde11e1d496
Edward Brekelbaum <nedbrek@yahoo.com>
2012-02-07 20:25:34 -0500

    Added sword::get to test

--- test.tcl
+++ test.tcl
@@ -1,6 +1,14 @@
 load ./tclsword.dll
 set mods [sword::modules]
 foreach d $mods {
-	puts "[dict get $d Name] [dict get $d Type]"
+	set name [dict get $d Name]
+	puts "$name [dict get $d Type]"
+
+	if {$name eq "ESV"} {
+		set l [sword::get $name "John 1:1-4"]
+		foreach d $l {
+			puts "[dict get $d Ref] [dict get $d Text]"
+		}
+	}
 }
 
