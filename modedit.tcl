bd3043638ef3846b27cbeb6c69ec7d1166fee64a
Edward Brekelbaum <nedbrek@yahoo.com>
2012-02-19 17:31:37 -0500

    Fixed bug on spinner validate (missing configure command)

--- modedit.tcl
+++ modedit.tcl
@@ -23,5 +23,5 @@ wm withdraw .
 toplevel .tModEdit
 pack [spinbox .tModEdit.sIdx -from 1 -to 5624 -validatecommand {validIdx %P}] -side top
 pack [text    .tModEdit.tT] -side top
-.tModEdit.sIdx -validate key
+.tModEdit.sIdx configure -validate key
 
