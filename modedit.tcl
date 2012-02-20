f60f0a8cbb5e6f268584f5d52eeddea615cf26fc
Edward Brekelbaum <nedbrek@yahoo.com>
2012-02-20 07:13:28 -0500

    Added Greek transliteration

--- modedit.tcl
+++ modedit.tcl
@@ -5,6 +5,64 @@ load ./tclsword.dll
 ###
 set module "StrongsGreek"
 
+set greekDict {
+	α a
+	β b
+	γ g
+	δ d
+	ε e
+	ζ z
+	η _ee
+	θ _th
+	ι i
+	κ k
+	λ l
+	μ m
+	ν n
+	ξ x
+	ο o
+	π p
+	ρ r
+	σ s
+	ς _s
+	τ t
+	υ u
+	φ _ph
+	ψ _ps
+	ω _oe
+}
+foreach {g e} $greekDict {
+	dict lappend revGreekDict $e $g
+}
+
+proc revGreek {s} {
+	set cur ""
+	foreach e [split $s ""] {
+		if {$cur eq ""} {
+			if {$e ne "_"} {
+				append ret [dict get $::revGreekDict $e]
+				continue
+			}
+			append cur $e
+			continue
+		}
+
+		if {$e eq "s"} {
+			append ret [dict get $::revGreekDict "_s"]
+			set cur ""
+			continue
+		}
+		append cur $e
+		if {[string length $cur] == 3} {
+			append ret [dict get $::revGreekDict $cur]
+			set cur ""
+			continue
+		}
+	}
+
+	return $ret
+}
+
 ###
 proc validIdx {newVal} {
 	if {$newVal <= 0 || $newVal > 5624} {
@@ -25,4 +83,12 @@ pack [spinbox .tModEdit.sIdx -from 1 -to 5624 -validatecommand {validIdx %P}] -s
 pack [text    .tModEdit.tT] -side top
 .tModEdit.sIdx configure -validate key
 .tModEdit.tT insert end [sword::dict $::module 1]
+
+toplevel .tTranslate
+pack [entry .tTranslate.e]
+bind .tTranslate.e <Return> {
+	set t [revGreek [.tTranslate.e get]]
+	.tTranslate.e delete 0 end
+	.tTranslate.e insert end $t
+}
 
